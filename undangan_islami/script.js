d1952a8ec721dacf833e4b9555bc6eeaef0c7e5f
rikodesign019-lang <rikodesign019@gmail.com>
2025-10-06 15:44:55 +0800

    Delete undangan_islami/script.js

--- undangan_islami/script.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', e => {
-    e.preventDefault();
-    document.querySelector(anchor.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
